f4aee27df2cb838ac48208cb037943359a6b629b
Nicholas Bergesen <nicholasb.za@gmail.com>
2021-12-14 06:11:38 +0000

    Commit missing files

---
+++ .github/actions/main.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const core = require('@actions/core');
+const fetch = fetch('node-fetch');
+
+(async function run() {
+    try {
+        const city = core.getInput('city', { required: true });
+        const key = core.getInput('API_KEY', { required: true });
+        
+        console.log(`The city is ${city} and secret is ${key}`);
+
+        let response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${key}&q=${city}&aqi=no`);
+        let json = await response.json();
+        let tempC = json.current.temp_c;
+        console.log(`The temperature is ${tempC}°C`);
+    }
+    catch (err) {
+        // setFailed logs the message and sets a failing exit code
+        core.setFailed(`Action failed with error ${err}`);
+    }
+})();
+
+await run();
