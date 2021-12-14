1ef431d2f0a9ae86293442f3573a774384d13293
Nicholas Bergesen <nicholasb.za@gmail.com>
2021-12-14 06:55:03 +0000

    Module syntax

--- .github/actions/main.js
+++ .github/actions/main.js
@@ -3,18 +3,18 @@
 const core = require('@actions/core');
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
-const main = () => {
+const main = async () => {
     try {
         const city = core.getInput('city', { required: true });
         const key = core.getInput('API_KEY', { required: true });
         
         console.log(`The city is ${city} and secret is ${key}`);
 
-        let tempC = fetch(`https://api.weatherapi.com/v1/current.json?key=${key}&q=${city}&aqi=no`)
-            .then(res => res.json())
-            .then(json => json.current.temp_c);
+        const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${key}&q=${city}&aqi=no`)
+        const json = await response.json();
+        const temp_c = json.current.temp_c;
 
-        console.log(`The temperature is ${tempC}°C`);
+        console.log(`The temperature is ${temp_c}°C`);
     }
     catch (err) {
         // setFailed logs the message and sets a failing exit code
