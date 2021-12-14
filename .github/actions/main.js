6c6f72b2d7780440d9b61fbd471aefb308782572
Nicholas Bergesen <nicholasb.za@gmail.com>
2021-12-14 06:39:17 +0000

    try module

--- .github/actions/main.js
+++ .github/actions/main.js
@@ -1,21 +1,28 @@
 'use strict'
 
-import core from '@actions/core';
-import fetch from "node-fetch";
+const test = () => {
+    import core from '@actions/core';
+    import fetch from "node-fetch";
 
-try {
-    const city = core.getInput('city', { required: true });
-    const key = core.getInput('API_KEY', { required: true });
-    
-    console.log(`The city is ${city} and secret is ${key}`);
 
-    let tempC = fetch(`https://api.weatherapi.com/v1/current.json?key=${key}&q=${city}&aqi=no`)
-        .then(res => res.json())
-        .then(json => json.current.temp_c);
+    try {
+        const city = core.getInput('city', { required: true });
+        const key = core.getInput('API_KEY', { required: true });
+        
+        console.log(`The city is ${city} and secret is ${key}`);
 
-    console.log(`The temperature is ${tempC}°C`);
+        let tempC = fetch(`https://api.weatherapi.com/v1/current.json?key=${key}&q=${city}&aqi=no`)
+            .then(res => res.json())
+            .then(json => json.current.temp_c);
+
+        console.log(`The temperature is ${tempC}°C`);
+    }
+    catch (err) {
+        // setFailed logs the message and sets a failing exit code
+        core.setFailed(`Action failed with error ${err}`);
+    }
 }
-catch (err) {
-    // setFailed logs the message and sets a failing exit code
-    core.setFailed(`Action failed with error ${err}`);
-}
+
+export default test;
+
+test();
