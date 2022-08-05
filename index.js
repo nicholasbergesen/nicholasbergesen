14a96e19832ded8934a287c5b1eb1e165edc03c7
Nicholas Bergesen <nicholasbergesen@github.com>
2022-08-05 15:50:13 +0000

    add script

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function start(color) {
 }
 
 function init(e) {
-    const color = (e.screenX * e.screenY).toString(16)
+    const color = ((e.screenX + 1) * (e.screenY + 1)).toString(16)
     start(color)
 }
