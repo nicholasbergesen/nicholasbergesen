0ee29cb86fa6678011d401dd4376221407cbf6f8
Nicholas Bergesen <nicholasbergesen@github.com>
2022-08-05 16:04:32 +0000

    add script

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function start(color) {
 }
 
 function init(e) {
-    const color = ((Math.abs(e.clientX + 1) * Math.abs(e.clientY + 1)) % 16777215).toString(16)
+    const color = ((Math.abs(e.screenX * 13) * Math.abs(e.screenY * 7)) % 16777215).toString(16)
     start(color)
 }
